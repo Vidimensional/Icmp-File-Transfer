6b84ee29196f8a7944f3c3bf585a0263d10c42f2
Daniel Vidal <dvidal@connectical.com>
2014-08-13 23:21:08 +0200

    using with statement to handle files

--- icmp.py
+++ icmp.py
@@ -16,7 +16,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#import socket
 import sys
  
 
@@ -28,38 +27,34 @@ filename = sys.argv[2]
 if action == 'send':
     dst_addr = sys.argv[3]
 
-#s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
 icmp_socket = IcmpSocket()
 
 if action == 'send': 
-    mode = 'r'
-    f = open(filename, mode)
-
-    seq_n = 0
-    while True: 
-        data = f.read(56)
-        if not data:
-            packet = IcmpPacket(ECHO_REQUEST, seq_n=seq_n, payload=data, 
-                                code=2)
+    with open(filename, 'r') as f:
+        seq_n = 0
+        while True: 
+            data = f.read(56)
+            if not data:
+                packet = IcmpPacket(ECHO_REQUEST, seq_n=seq_n, payload=data, 
+                                    code=2)
+                icmp_socket.sendto(packet, dst_addr)
+                break
+            packet = IcmpPacket(ECHO_REQUEST, seq_n=seq_n, payload=data)
             icmp_socket.sendto(packet, dst_addr)
-            break
-        packet = IcmpPacket(ECHO_REQUEST, seq_n=seq_n, payload=data)
-        icmp_socket.sendto(packet, dst_addr)
-        seq_n += 1
+            seq_n += 1
 
 elif action == 'recv': 
-    mode = 'w'
-    f = open(filename, mode)
-    buff = []
-    while True:
-        icmp = icmp_socket.recv()
-        if icmp.code is 2: break
-        buff.append((icmp.seq_n, icmp.payload))
-    buff.sort()
-    str_buff = ''
-    for elem in buff:
-        str_buff += elem[1]
-    f.write(str_buff) 
+    with open(filename, 'w') as f:
+        buff = []
+        while True:
+            icmp = icmp_socket.recv()
+            if icmp.code is 2: break
+            buff.append((icmp.seq_n, icmp.payload))
+        buff.sort()
+        str_buff = ''
+        for elem in buff:
+            str_buff += elem[1]
+        f.write(str_buff) 
 
     
 
