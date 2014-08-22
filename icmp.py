07463fbc150e5fb52c592b7ab7c5753b5f413eae
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2014-08-22 15:33:39 +0000

    protecting against lack of arguments

--- icmp.py
+++ icmp.py
@@ -19,19 +19,32 @@ import sys
 
 from ICMP.IcmpApp import IcmpSender, IcmpReceiver
 
-action = sys.argv[1]
-filename = sys.argv[2]
+
+def show_usage ():
+    print """
+      USAGE:
+        icmp.py recv <destination file>
+        icmp.py send <file to transfer> <remote address>
+    """
+    exit()
 
 
-if action == 'send': 
-    dst_addr = sys.argv[3]
-    with IcmpSender(filename) as sender:
-        sender.send(dst_addr)
+if __name__ == '__main__':
+    try:
+        action = sys.argv[1]
+        filename = sys.argv[2]
+    except IndexError:
+        show_usage()
 
-elif action == 'recv': 
-    with IcmpReceiver(filename) as receiver:
-        receiver.receive()
-    
+    if action == 'send': 
+        try:    dst_addr = sys.argv[3]
+        except: show_usage()
 
+        with IcmpSender(filename) as sender:
+            sender.send(dst_addr)
+
+    elif action == 'recv': 
+        with IcmpReceiver(filename) as receiver:
+            receiver.receive()
 
 
